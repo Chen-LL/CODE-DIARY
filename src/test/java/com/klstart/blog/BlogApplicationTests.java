a5dba98fa7a7aa23a9869adfa6816a8550e18fde
kunliang <liangzaia321@qq.com>
2021-11-22 17:20:15 +0800

    环境搭建，代码生成

---
+++ src/test/java/com/klstart/blog/BlogApplicationTests.java
@@ -0,0 +1,14 @@
+package com.klstart.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
